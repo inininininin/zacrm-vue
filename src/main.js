0b55cb6f6aa855c1d8d7bcc129e11392392266c0
onerunsall <413038044@qq.com>
2020-05-22 10:43:49 +0800

    rxw

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index.js'
-import '../static/js/rem.js'
 import ElementUI from 'element-ui';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import moment from 'moment'
-Vue.prototype.$version = '20.0521.1841'
+Vue.prototype.$version = 'rxw'
 Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
 Vue.config.productionTip = false
 Vue.prototype.moment = moment;
