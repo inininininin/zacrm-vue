853e2ac7f4bc41ad6a9c3e0978a25fb13d376ec3
onerunsall <413038044@qq.com>
2020-06-03 17:54:08 +0800

    20.0603.1754

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+Vue.prototype.$version = '20.0603.1754'
+Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
 import App from './App'
 import router from './router/index.js'
 import ElementUI from 'element-ui';
@@ -8,8 +10,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import moment from 'moment'
-Vue.prototype.$version = '20.0528.1819'
-Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
+
 Vue.config.productionTip = false
 Vue.prototype.moment = moment;
 Vue.prototype.$store = store
