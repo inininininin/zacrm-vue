63fd152365ed62786221b74de51cae764c3c0120
onerunsall <413038044@qq.com>
2020-07-23 14:54:42 +0800

    20.0723.1454

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-Vue.prototype.$version = '20.0722.0818'
+Vue.prototype.$version = '20.0723.1454'
 Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
 import App from './App'
 import router from './router/index.js'
