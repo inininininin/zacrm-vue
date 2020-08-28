c92cd83e78e2a0b2cb2ca8b2ea73eca6555725e0
inininininin <413038044@qq.com>
2020-08-28 10:58:52 +0800

    1.0.2008281058

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-Vue.prototype.$version = '1.0.2008262102'
+Vue.prototype.$version = '1.0.2008281058'
 Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
 import App from './App'
 import router from './router/index.js'
