afeebadf17e62b5e5cccc7b1d153294fc7913852
inininininin <413038044@qq.com>
2020-10-13 15:11:31 +0800

    20.10.13.2

--- src/main.js
+++ src/main.js
@@ -1,46 +1,39 @@
+/* eslint-disable no-new */
+/* eslint-disable import/first */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-Vue.prototype.$version = '1.0.1.2009221252'
-Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
-import App from './App'
-import router from './router/index.js'
+import Vue from 'vue';
+Vue.prototype.$version = '20.10.13.2';
+Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.';
+import App from './App';
+import router from './router/index.js';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-
 import * as echarts from 'echarts/lib/echarts';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
 import 'echarts/lib/component/toolbox';
-import callService from './assets/call'
-import common from './assets/common'
-
-
-import store from './store'
-import moment from 'moment'
-import axios from 'axios'
-import qs from 'qs'
+import callService from './assets/call';
+import common from './assets/common';
+import store from './store';
+import moment from 'moment';
+import axios from 'axios';
 
 axios.interceptors.request.use(
   config => {
     config.cancelToken = new axios.CancelToken(function (cancel) {
-      store.commit('pushToken', {cancelToken: cancel})
-    })
-    return config
+      store.commit('pushToken', {cancelToken: cancel});
+    });
+    return config;
   }
-)
+);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.prototype.moment = moment;
-Vue.prototype.$store = store
-Vue.prototype.$axios = axios
+Vue.prototype.$store = store;
+Vue.prototype.$axios = axios;
 Vue.prototype.$callService = callService;
 Vue.prototype.$common = common;
-
-
-// Vue.prototype.$laydate = laydate
-
 Vue.use(ElementUI);
 Vue.prototype.$echarts = require('echarts')
 new Vue({
@@ -49,4 +42,4 @@ new Vue({
   store,
   components: { App },
   template: '<App/>'
-})
+});
