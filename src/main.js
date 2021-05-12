75cf58f25ee302b26d883542438b99f5b3ea0eca
renx <413038044@qq.com>
2021-05-12 18:32:03 +0800

    1.5

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/first */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';  
+import Vue from 'vue';   
 Vue.prototype.$version = '1.5';
 Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.';
 Vue.prototype.$Interface = '';
